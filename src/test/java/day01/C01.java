fdbe81cf212410d79480f3d36053bbcab7495fa2
esracnykn00 <esrakaraoglan00@gmail.com>
2023-09-17 18:16:51 +0300

    "Esra-17.09.23-V1"

---
+++ src/test/java/day01/C01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+        System.out.println("deneme");
+        System.out.println("deneme");
+        System.out.println("deneme");
+    }
+}
